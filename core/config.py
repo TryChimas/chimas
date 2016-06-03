33cd7b2d12b26527d1c6cb57c11b415dedd58b4c
kassius iacchus <kassivs@gmail.com>
2016-06-03 18:24:21 -0300

    config and argparse beginning to be written

--- core/config.py
+++ core/config.py
@@ -7,22 +7,30 @@ from core import ROOT_PATH, ETC_PATH
 DEFAULT_CONFIG_FILEPATH = ETC_PATH + "/chimas-default.conf"
 USER_CONFIG_FILEPATH = ROOT_PATH + "/chimas.conf"
 
-with open(DEFAULT_CONFIG_FILEPATH) as default_config_file:
-    default_config = toml.loads(default_config_file.read())
+class CliParser:
 
-with open(USER_CONFIG_FILEPATH) as user_config_file:
-    user_config = toml.loads(user_config_file.read())
-
-chimas_config = default_config
-chimas_config.update(user_config)
-
-class ConfigParser:
-    def __init__(self, config=user_config):
-
-
-        pass
-
-class TranslateConfig:
+    def __init__():
+        self.parser = argparse(description = "Chimas Server")
+        parser.add_argument('-c', '--config', action='store', default=USER_CONFIG_FILEPATH)
 
     def servername(listen,port):
         return listen:port
+
+        # this function will allow to write directly to flask/eve app.config from user config
+        def configLiteral():
+            pass
+
+class ConfigParser:
+
+    def __init__(self):
+
+        with open(DEFAULT_CONFIG_FILEPATH) as default_config_file:
+            default_config = toml.loads(default_config_file.read())
+
+        with open(USER_CONFIG_FILEPATH) as user_config_file:
+            user_config = toml.loads(user_config_file.read())
+
+        chimas_config = default_config
+        chimas_config.update(user_config)
+
+        self.parse(chimas_config)
