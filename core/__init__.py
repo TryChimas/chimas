ecd98e845d39202c03f405f3055c3eff54a11881
kassius iacchus <kassivs@gmail.com>
2016-06-13 12:41:34 -0300

    commiting before cleaning code

--- core/__init__.py
+++ core/__init__.py
@@ -8,7 +8,7 @@ INCLUDE_PATH = ROOT_PATH + "inc/"
 ETC_PATH = ROOT_PATH + "etc/"
 
 from flask import Flask
-from flask_sqlalchmy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 #from eve import Eve
 
 #from eve_sqlalchemy import SQL
@@ -30,9 +30,9 @@ from sqlalchemy import (
 
 from sqlalchemy import func
 
-from .schemas.boards import boards_schema
-from .schemas.posts import posts_schema
-from .schemas.users import users_schema
+#from .schemas.boards import boards_schema
+#from .schemas.posts import posts_schema
+#from .schemas.users import users_schema
 
 #from flask import current_app as app
 #from flask import abort
@@ -41,14 +41,19 @@ from .schemas.users import users_schema
 
 Base = declarative_base()
 
-def get_class_by_tablename(table_fullname, base=Base):
-    for c in base._decl_class_registry.values():
-        if hasattr(c, '__table__') and c.__table__.fullname == table_fullname:
-            return c
-        else:
-            return None
+APP = Flask(__name__)
+APP.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{0}dummy.sqlite3-autocreate".format(ROOT_PATH)
 
-class CommonTable(Base):
+DB = SQLAlchemy(APP)
+
+#def get_class_by_tablename(table_fullname, base=Base):
+#    for c in base._decl_class_registry.values():
+#        if hasattr(c, '__table__') and c.__table__.fullname == table_fullname:
+#            return c
+#        else:
+#            return None
+
+class CommonTable(DB.Model):
     __abstract__ =  True
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -77,10 +82,6 @@ class Posts(CommonTable):
     post_text = Column(String)
     hash_id = Column(String)
 
-#def get_topic(self, topid_id):
-#    #posts = Posts.query.filter(Posts.id == topic_id).first()
-#    return "we are inside post item"
-
 class Users(CommonTable):
     __tablename__ = 'users'
 
@@ -99,39 +100,18 @@ class Roles(CommonTable):
     users = Column(String)
 
     def is_authorized(user, is_authenticated, allowed_roles, resource, method, lookup):
-
-        if 'anonymous' in allowed_roles:
-            return True
-
-        if 'registered' in allowed_roles:
-            if is_authenticated:
-                return True
-
-        if 'moderators' in allowed_roles:
-            pass
-
-        #if 'owner' in allowed_roles:
+        pass
 
 
-        #login = user.login
 
-#registerSchema('boards')(Boards)
-#registerSchema('posts')(Posts)
-#registerSchema('users')(Users)
-#Boards._eve_schema['boards'].update(boards_schema)
-#Posts._eve_schema['posts'].update(posts_schema)
-#Users._eve_schema['users'].update(users_schema)
 
-#DOMAIN = {
-#    'boards' : Boards._eve_schema['boards'],
-#    'posts'  : Posts._eve_schema['posts'],
-#    'users'  : Users._eve_schema['users'],
-#}
-
-SQLALCHEMY_DATABASE_URI = "sqlite:///{0}dummy.sqlite3-autocreate".format(ROOT_PATH)
+#SQLALCHEMY_DATABASE_URI = "sqlite:///{0}dummy.sqlite3-autocreate".format(ROOT_PATH)
 
 #APP = Eve(settings=ETC_PATH+'eve-settings.py', auth=ChimasAuth, validator=ValidatorSQL, data=SQL)
-APP = Flask(__name__)
+#APP = Flask(__name__)
+#APP.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{0}dummy.sqlite3-autocreate".format(ROOT_PATH)
+
+#DB.init_app(APP)
 #ConfigParser(APP)
 
 #class Chimas(Flask):
@@ -151,6 +131,6 @@ def get_topic(self, topid_id):
 
 #Base.metadata.bind = APP.data.driver.engine
 #APP.data.driver.Model = Base
-APP.data.driver.create_all()
+DB.create_all()
 
-Base.query = APP.data.driver.session.query_property()
+#Base.query = DB.session.query_property()
