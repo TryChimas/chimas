77692fd6b78703dfe7e368ee6997078f85868bdf
kassius iacchus <kassivs@gmail.com>
2016-06-13 15:44:13 -0300

    Marshmallow beginning to work

--- core/__init__.py
+++ core/__init__.py
@@ -9,6 +9,7 @@ ETC_PATH = ROOT_PATH + "etc/"
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 #from .auth import ChimasAuth
 
@@ -27,19 +28,20 @@ from sqlalchemy import func
 
 Base = declarative_base()
 
-APP = Flask(__name__)
+#APP = Flask(__name__)
+
+class Chimas(Flask):
+
+    def __init__(self, import_name=__package__, **kwargs):
+        super(Chimas, self).__init__(import_name, **kwargs)
+
+APP = Chimas(__name__)
+
 APP.config['DEBUG'] = True
 APP.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{0}dummy.sqlite3-autocreate".format(ROOT_PATH)
 
-#class Chimas(Flask):
-
-#    def __init__(self, import_name=__package__, **kwargs):
-#        super(Chimas, self).__init__(import_name, **kwargs)
-
-
-#APP = Chimas(__name__)
-
 DB = SQLAlchemy(APP)
+MA = Marshmallow(APP)
 
 class CommonTable(DB.Model):
     __abstract__ =  True
@@ -50,12 +52,20 @@ class CommonTable(DB.Model):
     etag = Column(String)
     deleted = Column(String)
 
+#class CommonSchema(MA.ModelSchema):
+#    class Meta:
+#        model = CommonTable
+
 class Boards(CommonTable):
     __tablename__ = 'boards'
 
     id = None
     title = Column(String, primary_key=True, unique=True)
     description = Column(String)
+
+class BoardsSchema(MA.ModelSchema):
+    class Meta:
+        model = Boards
 
 class Posts(CommonTable):
     __tablename__ = 'posts'
@@ -69,6 +79,10 @@ class Posts(CommonTable):
     title = Column(String)
     post_text = Column(String)
     hash_id = Column(String)
+
+class PostsSchema(MA.ModelSchema):
+    class Meta:
+        model = Posts
 
 class Users(CommonTable):
     __tablename__ = 'users'
@@ -93,6 +107,7 @@ class Roles(CommonTable):
 @APP.route('/posts/<int:topic_id>')
 def get_topic(topic_id):
     #posts = Posts.query.filter(Posts.id == topic_id).first()
-    return "we are inside post item"
+    posts = Posts.get(topic_id)
+    return posts
 
 DB.create_all()
