737a854d6710ca71cb14040cb70e3757abc5668b
kassius iacchus <kassivs@gmail.com>
2016-06-13 12:55:27 -0300

    removed many of the commented code.

--- app.py
+++ app.py
@@ -1,37 +1,9 @@
 import sys
 sys.path.insert(1, sys.path[0]+'/inc/')
 
-
-#print(sys.path)
-
-#from eve import Eve
-
-#from eve_sqlalchemy import SQL
-#from eve_sqlalchemy.validation import ValidatorSQL
-
-#from core.auth import ChimasAuth
-#from core import Base as Base
-
-#from core import CommonTable
-#from core import Users, Boards, Posts
-
-#from core.config import ConfigParser
-
-#app = Eve(settings='etc/eve-settings.py', auth=ChimasAuth, validator=ValidatorSQL, data=SQL)
-#ConfigParser(app)
-
-#print(app.config)
-#import pprint
-#pprint.pprint(app.config, width=1)
-
 from core import APP
 
-#Base.metadata.bind = app.data.driver.engine
-#app.data.driver.Model = Base
-#app.data.driver.create_all()
-
-#Base.query = app.data.driver.session.query_property()
-
 if __name__ == "__main__":
-
+    #import pprint
+    #pprint.pprint(app.config, width=1)
     APP.run();
--- core/__init__.py
+++ core/__init__.py
@@ -9,17 +9,12 @@ ETC_PATH = ROOT_PATH + "etc/"
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from eve import Eve
-
-#from eve_sqlalchemy import SQL
-#from eve_sqlalchemy.validation import ValidatorSQL
 
 from .auth import ChimasAuth
 
 from .config import ConfigParser
 
 from sqlalchemy.ext.declarative import declarative_base
-#from eve_sqlalchemy.decorators import registerSchema
 
 from sqlalchemy import (
         Column,
@@ -30,28 +25,21 @@ from sqlalchemy import (
 
 from sqlalchemy import func
 
-#from .schemas.boards import boards_schema
-#from .schemas.posts import posts_schema
-#from .schemas.users import users_schema
-
-#from flask import current_app as app
-#from flask import abort
-
-#APP = Eve(settings=ETC_PATH+'eve-settings.py', auth=ChimasAuth, validator=ValidatorSQL, data=SQL)
-
 Base = declarative_base()
 
 APP = Flask(__name__)
+APP.config['DEBUG'] = True
 APP.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{0}dummy.sqlite3-autocreate".format(ROOT_PATH)
 
-DB = SQLAlchemy(APP)
+#class Chimas(Flask):
 
-#def get_class_by_tablename(table_fullname, base=Base):
-#    for c in base._decl_class_registry.values():
-#        if hasattr(c, '__table__') and c.__table__.fullname == table_fullname:
-#            return c
-#        else:
-#            return None
+#    def __init__(self, import_name=__package__, **kwargs):
+#        super(Chimas, self).__init__(import_name, **kwargs)
+
+
+#APP = Chimas(__name__)
+
+DB = SQLAlchemy(APP)
 
 class CommonTable(DB.Model):
     __abstract__ =  True
@@ -102,35 +90,9 @@ class Roles(CommonTable):
     def is_authorized(user, is_authenticated, allowed_roles, resource, method, lookup):
         pass
 
-
-
-
-#SQLALCHEMY_DATABASE_URI = "sqlite:///{0}dummy.sqlite3-autocreate".format(ROOT_PATH)
-
-#APP = Eve(settings=ETC_PATH+'eve-settings.py', auth=ChimasAuth, validator=ValidatorSQL, data=SQL)
-#APP = Flask(__name__)
-#APP.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{0}dummy.sqlite3-autocreate".format(ROOT_PATH)
-
-#DB.init_app(APP)
-#ConfigParser(APP)
-
-#class Chimas(Flask):
-
-#    def __init__(self, import_name=__package__, **kwargs):
-#        super(Chimas, self).__init__(import_name, **kwargs)
-
-
-#APP = Chimas(__name__)
-
-#Db = SQLAlchemy(APP, metadata=Base.metadata)
-
 @APP.route('/posts/<int:topic_id>')
 def get_topic(self, topid_id):
     #posts = Posts.query.filter(Posts.id == topic_id).first()
     return "we are inside post item"
 
-#Base.metadata.bind = APP.data.driver.engine
-#APP.data.driver.Model = Base
 DB.create_all()
-
-#Base.query = DB.session.query_property()
