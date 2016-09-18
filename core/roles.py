a45fc0d3fb214db41c2348316a9854311af9e931
Iacchus Mercurius <kassivs@gmail.com>
2016-09-18 17:38:27 -0300

    goingo on..

--- core/roles.py
+++ core/roles.py
@@ -12,24 +12,24 @@ from marshmallow import fields, Schema
 
 from flask import request
 
-class Groups(CommonTable):
-    __tablename__ = 'groups'
+class Roles(CommonTable):
+    __tablename__ = 'roles'
 
     #id = None
-    role = Column(String, primary_key=True, unique=True)
+    role = Column(String)
     arguments = Column(String) # FIXME: arguments is best if a json/dict,eg. {'board_id':'41'}
     username = Column(String)
 
-@APP.route('/groups', methods=['GET'])
-def list_groups():
+@APP.route('/roles', methods=['GET'])
+def list_roles():
     pass
 
-@APP.route('/groups/<string:role>/<string:arguments>', methods=['GET'])
-def fetch_group_info():
+@APP.route('/roles/<string:role>/<string:arguments>', methods=['GET'])
+def fetch_role_info():
     pass
 
-@APP.route('/groups/by_username/<string:username>', methods=['GET'])
-def list_user_groups():
+@APP.route('/roles/by_username/<string:username>', methods=['GET'])
+def list_user_roles():
     pass
 
 @APP.route('/groups/addrole', methods=['POST'])
