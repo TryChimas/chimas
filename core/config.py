77e2749b6e101cf35bd71c35f48190b3a740ce9f
Iacchus Mercurius <kassivs@gmail.com>
2016-09-20 14:36:04 -0300

    config dev

--- core/config.py
+++ core/config.py
@@ -45,51 +45,16 @@ def parse_args(required_args=[], optional_args=[], config_dict=chimas_config, ap
                 if optional_arg in config_dict:
                     optional.update({optional_arg : config_dict[optional_arg]})
 
+            #kwargs.update( {'required':required});
+            #kwargs['optional'] = optional
+
             return configuration_parser_function(required, optional, *args, **kwargs)
         return actual_function
     return actual_decorator
 
-@parse_args(required_args=['listen','port'],config_dict=chimas_config)
+@parse_args(required_args=['listen','port'], config_dict=chimas_config)
 #def parse_server_name( listen=config_dict['listen'], port=config_dict['port'],app_config=app_config):
-def parse_server_name( req_args, opt_args ):
-
-    APP.config['SERVER_NAME'] = ":".join( [str(req_args['listen']), str(req_args['port'])])
+def parse_server_name( req, opt, *args, **kwargs):
+    APP.config['SERVER_NAME'] = req['listen'] + ":" + str(req['port'])
 
 parse_server_name()
-
-# cliparser = argparse.ArgumentParser(description = "Chimas Server")
-# cliparser.add_argument(
-#     '-c', '--config',
-#         action='store',
-#         dest='config_filename',
-#         default=USER_CONFIG_FILEPATH,
-#         help="Selects user configuration filename." )
-#
-# args = cliparser.parse_args()
-
-# class ConfigParser:
-#
-#     def __init__(self, app):
-#
-# print("Opening default config file: {0}".format(DEFAULT_CONFIG_FILEPATH))
-# with open(DEFAULT_CONFIG_FILEPATH) as default_config_file:
-#     default_config = toml.loads(default_config_file.read())
-#
-#         if args.config_filename:
-#             print("Opening config file: {0}".format(args.config_filename))
-#             with open(args.config_filename) as user_config_file:
-#                 user_config = toml.loads(user_config_file.read())
-#         else:
-#             print("Opening config file: {0}".format(USER_CONFIG_FILEPATH))
-#             with open(USER_CONFIG_FILEPATH) as user_config_file:
-#                 user_config = toml.loads(user_config_file.read())
-#
-#         chimas_config = default_config
-#         chimas_config.update(user_config)
-#
-#         self.parse_config(app, chimas_config)
-#
-#     def parse_config(self, app, config):
-#
-#         if 'listen' in config and 'port' in config:
-#             app.config['SERVER_NAME'] = "{0}:{1}".format(config['listen'], config['port'])
