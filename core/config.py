f6607e8c297d800712b715d01b2331e311b28b35
Iacchus Mercurius <kassivs@gmail.com>
2016-09-22 09:30:57 -0300

    ok; probably not passing

--- core/config.py
+++ core/config.py
@@ -4,8 +4,6 @@ from core import APP, ROOT_PATH, ETC_PATH
 import click
 import toml
 
-from functools import wraps
-
 DEFAULT_CONFIG_FILEPATH = ETC_PATH + "chimas-default.conf"
 USER_CONFIG_FILEPATH = ROOT_PATH + "chimas.conf"
 
@@ -16,38 +14,50 @@ def set_configuration_file(config):
 
 print("Opening default config file: {0}".format(DEFAULT_CONFIG_FILEPATH))
 with open(DEFAULT_CONFIG_FILEPATH) as default_config_file:
-    default_config = toml.loads(default_config_file.read())
+    default_config_params = toml.loads(default_config_file.read())
 
 print("Opening user config file: {0}".format(USER_CONFIG_FILEPATH))
 with open(USER_CONFIG_FILEPATH) as user_config_file:
-    user_config = toml.loads(user_config_file.read())
+    user_config_params = toml.loads(user_config_file.read())
 
-chimas_config = default_config
-chimas_config.update(user_config)
+config_params = default_config_params.update(user_config_params)
 
-def parse_args(required_args=[], optional_args=[], config_dict=chimas_config, app_config=APP.config):
-    def actual_decorator(configuration_parser_function):
-        @wraps(configuration_parser_function)
-        def actual_function(*args, **kwargs):
-            required = {}
-            optional = {}
+app_config = {}
+chimas_config = {}
 
-            for required_arg in required_args:
-                if not required_arg in config_dict:
-                    return False
-                else:
-                    required.update({required_arg : config_dict[required_arg]})
+class ChimasConfig:
+    def __init__(self):
+        self.function_map = {}
+        self.context_map = {}
+        self.option_map = {}
 
-            for optional_arg in optional_args:
-                if optional_arg in config_dict:
-                    optional.update({optional_arg : config_dict[optional_arg]})
+    def opt(self, option, short, descripton, default):
+        self.option_map[option].update({
+        option:{
+            'option' :option,
+            'short' : short,
+            'description': description,
+            'default': default
+        }})
 
-            return configuration_parser_function(required, optional, *args, **kwargs)
-        return actual_function
-    return actual_decorator
+    def parser(self, context, required_args):
+        def function_wrapper(f):
+            self.function_map[context] = f
+            self.context_map[context] = { 'required_args': required_args }
 
-@parse_args(required_args=['listen','port'], config_dict=chimas_config)
-def parse_server_name( req, opt, *args, **kwargs):
-    APP.config['SERVER_NAME'] = req['listen'] + ":" + str(req['port'])
+            return f
+        return function_wrapper
 
-parse_server_name()
+    def parse_config(self, context):
+        func = self.function_map.get(context, None)
+        if not func:
+            pass
+        return func()
+
+config_parser = ChimasConfig()
+
+@config_parser.parser(context='servername', args=['listen','port'])
+def parse_server_name(listen=None, port=None):
+    app_config['SERVER_NAME'] = req['listen'] + ":" + str(req['port'])
+
+config_parser.parse_config()
