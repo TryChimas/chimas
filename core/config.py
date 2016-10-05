b9cf3916c27899f62487735a87fe8b2a6069ff74
Iacchus Mercurius <kassivs@gmail.com>
2016-10-05 20:17:46 -0300

    parser to configobj

--- core/config.py
+++ core/config.py
@@ -2,7 +2,8 @@
 from core import ROOT_PATH, ETC_PATH
 
 import click
-import toml
+#import toml
+import configobj
 
 # DEFAULT_CONFIG_FILEPATH = ETC_PATH + "chimas-default.conf"
 # USER_CONFIG_FILEPATH = ROOT_PATH + "chimas.conf"
@@ -20,41 +21,41 @@ import toml
 # with open(USER_CONFIG_FILEPATH) as user_config_file:
 #     user_config_params = toml.loads(user_config_file.read())
 
-config_params.update(user_config_params)
-print(config_params)
+#config_params.update(user_config_params)
+#print(config_params)
 
 app_config = {}
 chimas_config = {}
-default_config = ImmutableDict({
-    'DEBUG':                                get_debug_flag(default=False),
-    'TESTING':                              False,
-    'PROPAGATE_EXCEPTIONS':                 None,
-    'PRESERVE_CONTEXT_ON_EXCEPTION':        None,
-    'SECRET_KEY':                           None,
-    'PERMANENT_SESSION_LIFETIME':           timedelta(days=31),
-    'USE_X_SENDFILE':                       False,
-    'LOGGER_NAME':                          None,
-    'LOGGER_HANDLER_POLICY':               'always',
-    'SERVER_NAME':                          None,
-    'APPLICATION_ROOT':                     None,
-    'SESSION_COOKIE_NAME':                  'session',
-    'SESSION_COOKIE_DOMAIN':                None,
-    'SESSION_COOKIE_PATH':                  None,
-    'SESSION_COOKIE_HTTPONLY':              True,
-    'SESSION_COOKIE_SECURE':                False,
-    'SESSION_REFRESH_EACH_REQUEST':         True,
-    'MAX_CONTENT_LENGTH':                   None,
-    'SEND_FILE_MAX_AGE_DEFAULT':            timedelta(hours=12),
-    'TRAP_BAD_REQUEST_ERRORS':              False,
-    'TRAP_HTTP_EXCEPTIONS':                 False,
-    'EXPLAIN_TEMPLATE_LOADING':             False,
-    'PREFERRED_URL_SCHEME':                 'http',
-    'JSON_AS_ASCII':                        True,
-    'JSON_SORT_KEYS':                       True,
-    'JSONIFY_PRETTYPRINT_REGULAR':          True,
-    'JSONIFY_MIMETYPE':                     'application/json',
-    'TEMPLATES_AUTO_RELOAD':                None,
-})
+# default_config = ImmutableDict({
+#     'DEBUG':                                get_debug_flag(default=False),
+#     'TESTING':                              False,
+#     'PROPAGATE_EXCEPTIONS':                 None,
+#     'PRESERVE_CONTEXT_ON_EXCEPTION':        None,
+#     'SECRET_KEY':                           None,
+#     'PERMANENT_SESSION_LIFETIME':           timedelta(days=31),
+#     'USE_X_SENDFILE':                       False,
+#     'LOGGER_NAME':                          None,
+#     'LOGGER_HANDLER_POLICY':               'always',
+#     'SERVER_NAME':                          None,
+#     'APPLICATION_ROOT':                     None,
+#     'SESSION_COOKIE_NAME':                  'session',
+#     'SESSION_COOKIE_DOMAIN':                None,
+#     'SESSION_COOKIE_PATH':                  None,
+#     'SESSION_COOKIE_HTTPONLY':              True,
+#     'SESSION_COOKIE_SECURE':                False,
+#     'SESSION_REFRESH_EACH_REQUEST':         True,
+#     'MAX_CONTENT_LENGTH':                   None,
+#     'SEND_FILE_MAX_AGE_DEFAULT':            timedelta(hours=12),
+#     'TRAP_BAD_REQUEST_ERRORS':              False,
+#     'TRAP_HTTP_EXCEPTIONS':                 False,
+#     'EXPLAIN_TEMPLATE_LOADING':             False,
+#     'PREFERRED_URL_SCHEME':                 'http',
+#     'JSON_AS_ASCII':                        True,
+#     'JSON_SORT_KEYS':                       True,
+#     'JSONIFY_PRETTYPRINT_REGULAR':          True,
+#     'JSONIFY_MIMETYPE':                     'application/json',
+#     'TEMPLATES_AUTO_RELOAD':                None,
+# })
 
 class ChimasConfig:
     def __init__(self, config_params):
